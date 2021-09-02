c5f5740d894260cbb2e178ca7d42bb35590d0b49
Adam S <sethadam1@users.noreply.github.com>
2021-09-02 15:52:14 -0400

    Update PdoFish.php

--- PdoFish.php
+++ PdoFish.php
@@ -2,7 +2,4 @@
 
 require __DIR__."/credentials.php";
 require __DIR__."/PdoFish.class.php";
-foreach(glob(__DIR__.'/models/*.php') as $p) {
-	include_once($p);
-}
 PdoFish::startup($PdoFish_options);
