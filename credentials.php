c854c0a5a4774e236c0153cf0d751a003fa21637
Adam S <sethadam1@users.noreply.github.com>
2021-09-02 15:53:38 -0400

    Update credentials.php

--- credentials.php
+++ credentials.php
@@ -1,6 +1,7 @@
 <?php
 
-// PdoFish connection options
+// PdoFish setup options
+// if you specify a model path, PdoFish will setup all of your models
 $PdoFish_options = [
 	'username' => 'username',
 	'database' => 'database_name',  
@@ -8,6 +9,7 @@ $PdoFish_options = [
   	'type' => 'mysql',              // optional, defaults to mysql
 	'charset' => 'utf8',            // optional, defaults to utf8
 	'host' => "localhost",          // optional, defaults to localhost
-	'port' => '3306'                // optional, defaults to 3306
+	'port' => '3306',               // optional, defaults to 3306
+	'model_path'=>'/pathtomodels/',	// optional, defaults to null 
 ];
 
